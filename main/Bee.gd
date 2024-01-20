930a55060a67df84c0f8cfdb4131771079b4e7f0
Neruthes <i@neruthes.xyz>
2024-01-20 03:26:14 +0000

    Regular update '2024-01-20T03:26:14+00:00'

--- main/Bee.gd
+++ main/Bee.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 class_name Bee
 
-var master : Hive = null
+var master : Hive = null # Refer to Hive.gd, which preloads Bee.tscn, where Bee.gd is attached
 
 
 func _ready():
--- main/Hive.gd
+++ main/Hive.gd
@@ -9,8 +9,9 @@ func _ready():
 		spawn_bee()
 
 func spawn_bee():
-	var bee: Bee = bee_tscn.instantiate()
-	bee.global_position.x = randf_range(100,900)
-	bee.global_position.y = randf_range(100,500)
-	bee.master = self
-	add_child(bee)
+	var bee1: Bee = bee_tscn.instantiate() # This has no bug
+	var bee2: Bee = preload("res://main/Bee.tscn").instantiate() # This should cause bug!
+	bee2.global_position.x = randf_range(100,900)
+	bee2.global_position.y = randf_range(100,500)
+	bee2.master = self
+	add_child(bee2)
